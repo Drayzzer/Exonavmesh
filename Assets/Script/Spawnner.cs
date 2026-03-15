975f6e22420c7363f0eb19b670626bb20be057dc
Drayzzer <j.naval@nouvellesimages.xyz>
2026-03-15 22:37:15 +0100

    added some navmesh and spawnner

--- Assets/Script/Spawnner.cs
+++ Assets/Script/Spawnner.cs
@@ -1,11 +1,36 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
-namespace Script
+public class TopDownStupidEnnemiSpawner : MonoBehaviour
 {
-    public class Spawnner : MonoBehaviour
+    [SerializeField] private Vector3[] _spawnPoints;
+    [SerializeField] private float _spawningRange = 2;
+    [SerializeField] private float _spawningDelay = 5;
+    [SerializeField] private GameObject _ptibout;
+
+
+    private float _spawningTimer;
+
+    private bool _canSpawn
     {
-        [SerializeField] Transform _spawnPoints1;
-        [SerializeField] Transform _spawnPoints2;
+        get => _spawningTimer > _spawningDelay;
+    }
+
+    void Update()
+    {
+        if (_canSpawn)
+        {
+            Vector3 pos = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+            pos += new Vector3(Random.Range(-_spawningRange, _spawningRange)
+                , 0, Random.Range(-_spawningRange, _spawningRange));
+            _ptibout = Instantiate(_ptibout, pos, Quaternion.identity);
+            _spawningTimer = 0;
+        }
+        else
+        {
+            _spawningTimer += Time.deltaTime;
+        }
     }
 }
